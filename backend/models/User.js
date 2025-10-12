4c62a1d31d23f8dc35b5350c49ee9cc3f0f47e2d
Abdul Hannan <abdulhannanhere@gmail.com>
2025-10-12 21:47:11 +0500

    User aand Task Schema Created

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    prifileImageUrl: { type: String, default: null },
+    role: { type: String, enum: ["member", "admin"], default: "member" }, // Role-based access
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
