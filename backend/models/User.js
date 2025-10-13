430d4b068ad31a294c2ad6313c0a417b529729e7
Abdul Hannan <abdulhannanhere@gmail.com>
2025-10-14 01:26:00 +0500

    Auth api's completed

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    prifileImageUrl: { type: String, default: null },
+    profileImageUrl: { type: String, default: null },
     role: { type: String, enum: ["member", "admin"], default: "member" }, // Role-based access
   },
   { timestamps: true }
