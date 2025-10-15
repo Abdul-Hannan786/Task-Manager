3eabbd312f05d308756a0d7854462aa99874047a
Abdul Hannan <abdulhannanhere@gmail.com>
2025-10-15 22:07:59 +0500

    User API's completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { adminOnly, protect } from "../middlewares/authMiddleware.js";
-import { deleteUser, getUserById, getUsers } from "../controllers/userController.js";
+import { getUserById, getUsers } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // User Management Routes
 router.get("/", protect, adminOnly, getUsers);   // Get All Users (Admin Only)
 router.get("/:id", protect, adminOnly, getUserById);   // Get Specific User
-router.delete("/:id", protect, adminOnly, deleteUser); // Delete User (Admin Only)
 
 export default router;
