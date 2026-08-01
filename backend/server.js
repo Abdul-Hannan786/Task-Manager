cd1be816183fdbdb3f5c57bbd255a8fb87b9c64a
Abdul Hannan <abdulhannanhere@gmail.com>
2026-08-02 04:01:54 +0500

    minor update in the image upload

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/reports", reportRoutes);
 
+app.use("/uploads", express.static(Path.join(__dirname, "uploads")))
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
