e12334bb8a968112077fb7813a05baf7f06e4bb4
Abdul Hannan <abdulhannanhere@gmail.com>
2026-08-01 05:34:47 +0500

    Login api connected

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import AuthLayout from "../../components/layouts/AuthLayout";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosinstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -27,7 +29,32 @@ const Login = () => {
     setError("");
     setLoading(true);
 
-  // Login API Call
+    // Login API Call
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+
+      const { token, role } = response.data;
+      if (token) {
+        localStorage.setItem("token", token);
+
+        if (role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/dashboard");
+        }
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong");
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
--- frontend/src/utils/axiosinstance.js
+++ frontend/src/utils/axiosinstance.js
@@ -26,32 +26,26 @@ axiosInstance.interceptors.request.use(
 
 // Response Interceptor
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
+  (response) => response,
   (error) => {
-    if (error.code === "ECONNABORTED") {
-      console.error("Request timeout. Please try again.");
-    }
-
     if (error.response) {
       if (error.response.status === 401) {
         console.error("Status:", error.response.status);
         console.error("Response:", error.response.data);
 
-        window.location.href = "/login";
+        if (!error.config.url.includes("/login")) {
+          window.location.href = "/login";
+        }
       }
 
       if (error.response.status === 500) {
         console.error("Status:", error.response.status);
         console.error("Response:", error.response.data);
       }
-    } else {
-      console.error("Network error:", error.message);
     }
 
     return Promise.reject(error);
-  }
+  },
 );
 
 export default axiosInstance;
