98c23295661cbb8cf1f626238e1ebbf12dc9eedb
Abdul Hannan <abdulhannanhere@gmail.com>
2026-08-02 05:20:24 +0500

    Navbar completed

---
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SideMenu = ({ activeMeu }) => {
+  return <div>SideMenu</div>;
+};
+
+export default SideMenu;
--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
+import { UserContext } from "../../context/userContext";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
 
 const Dashboard = () => {
   useUserAuth();
-  return <div>Dashboard</div>;
+  const { user } = useContext(UserContext);
+  return <DashboardLayout>Dashboard</DashboardLayout>;
 };
 
 export default Dashboard;
