88e7f926d1d6ddeaaa6adfee86470479761a126a
Abdul Hannan <abdulhannanhere@gmail.com>
2026-08-02 06:32:33 +0500

    Admin Dashboard Data API connected

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,11 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContext";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosinstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Dashboard = () => {
   useUserAuth();
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
   const { user } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA,
+      );
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching Data", error);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardData();
+    return () => {};
+  }, []);
+
   return <DashboardLayout activeMenu="Dashboard">Dashboard</DashboardLayout>;
 };
 
