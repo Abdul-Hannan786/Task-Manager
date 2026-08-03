e735adf4cd29860a3c342b5433ceda40c477914f
Abdul Hannan <abdulhannanhere@gmail.com>
2026-08-04 04:58:08 +0500

    Working on Create Task Form

---
+++ frontend/src/components/Inputs/SelectDropdown.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SelectDropdown = ({option, value, onChange, placeholder}) => {
+  return (
+    <div>SelectDropdown</div>
+  )
+}
+
+export default SelectDropdown
--- frontend/src/pages/Admin/CreateTask.jsx
+++ frontend/src/pages/Admin/CreateTask.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useLocation } from "react-router-dom";
+import { LuTrash2 } from "react-icons/lu";
+import { PRIORITY_DATA } from "../../utils/data";
+import SelectDropdown from "../../components/Inputs/SelectDropdown";
 
 const CreateTask = () => {
   const location = useLocation();
@@ -37,7 +40,86 @@ const CreateTask = () => {
     });
   };
 
-  return <DashboardLayout activeMenu="Create Task"></DashboardLayout>;
+  // Create Task
+  const createTask = async () => {};
+
+  const updateTask = async () => {};
+
+  const handleSubmit = async () => {};
+
+  // Get Task info by ID
+  const getTaskDetailsByID = async () => {};
+
+  const deleteTask = () => {};
+
+  return (
+    <DashboardLayout activeMenu="Create Task">
+      <div className="mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+          <div className="form-card col-span-3">
+            <div className="flex items-center  justify-between">
+              <h2 className="text-xl md:text-2xl font-medium">
+                {taskId ? "Update Task" : "Create Task"}
+              </h2>
+
+              {taskId && (
+                <button
+                  className="flex items-center gap-1.5 text-[13px] font-medium text-rose-500 bg-rose-50 rounded px-2 py-1 border border-rose-100 hover:border-rose-300 cursor-pointer"
+                  onClick={() => setOpenDeleteAlert(true)}
+                >
+                  <LuTrash2 className="text-base" /> Delete
+                </button>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <label className="text-sm font-medium text-slate-600">
+                Task Title
+              </label>
+              <input
+                type="text"
+                placeholder="Create App UI"
+                className="form-input"
+                value={taskData.title}
+                onChange={({ target }) =>
+                  handleValueChange("title", target.value)
+                }
+              />
+            </div>
+
+            <div className="mt-3">
+              <label className="text-sm font-medium text-slate-600">
+                Description
+              </label>
+              <textarea
+                placeholder="Describe Task"
+                className="form-input"
+                rows={4}
+                value={taskData.description}
+                onChange={({ target }) =>
+                  handleValueChange("description", target.value)
+                }
+              />
+            </div>
+
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-span-6 md:col-span-4">
+                <label className="text-sm font-medium text-slate-600">
+                  Priority
+                </label>
+                <SelectDropdown
+                  options={PRIORITY_DATA}
+                  value={taskData.priority}
+                  onChange={(value) => handleValueChange("priority", value)}
+                  placeholder="Select Priority"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default CreateTask;
