68b513b40ee053c8366484e0d696dbf71fe567dd
Abdul Hannan <abdulhannanhere@gmail.com>
2026-08-04 06:53:36 +0500

    Working on AssignedTo Modal

--- frontend/src/components/Inputs/SelectUser.jsx
+++ frontend/src/components/Inputs/SelectUser.jsx
@@ -3,6 +3,7 @@ import axiosInstance from "../../utils/axiosinstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import { LuUsers } from "react-icons/lu";
 import Modal from "../Modal";
+import PROFILE_IMG from "../../assets/images/user.png";
 
 const SelectUser = ({ selectedUsers, setSelectedUsers }) => {
   const [allUsers, setAllUsers] = useState([]);
@@ -62,7 +63,39 @@ const SelectUser = ({ selectedUsers, setSelectedUsers }) => {
         onClose={() => setIsModalOpen(false)}
         title="Select Members"
       >
-        <div className="space-y-4 h-[60vh] overflow-y-auto"></div>
+        <div className="space-y-4 h-[60vh] overflow-y-auto">
+          {allUsers.map((user) => (
+            <div
+              key={user._id}
+              className="flex items-center gap-4 p-3 border-b border-gray-200"
+            >
+              <img
+                src={user.profileImageUrl || PROFILE_IMG}
+                alt={user.name}
+                className="w-10 h-10 object-cover rounded-full"
+              />
+              <div className="flex-1">
+                <p className="font-medium text-gray-800">{user.name}</p>
+                <p className="text-[13px] text-gray-500">{user.email}</p>
+              </div>
+              <input
+                type="checkbox"
+                checked={tempSelectedUsers.includes(user._id)}
+                onChange={() => toggleUserSelection(user._id)}
+                className="w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="flex justify-end gap-4 pt-4">
+          <button className="card-btn" onClick={() => setIsModalOpen(false)}>
+            CANCEL
+          </button>
+          <button className="card-btn-fill" onClick={handleAssign}>
+            ASSIGN
+          </button>
+        </div>
       </Modal>
     </div>
   );
