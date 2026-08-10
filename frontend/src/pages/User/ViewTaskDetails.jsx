781589dc3ab0e4380d980b36d920a7e14dda005c
Abdul Hannan <abdulhannanhere@gmail.com>
2026-08-10 06:25:44 +0500

    Working on Update Todo form

--- frontend/src/pages/User/ViewTaskDetails.jsx
+++ frontend/src/pages/User/ViewTaskDetails.jsx
@@ -108,7 +108,7 @@ const ViewTaskDetails = () => {
                   Todo Checklist
                 </label>
                 {task?.todoCheckList?.map((item, index) => (
-                  <TodoCheclist
+                  <TodoChecklist
                     key={`todo_${index}`}
                     text={item?.text}
                     isChecked={item?.completed}
@@ -136,5 +136,20 @@ const InfoBox = ({ label, value }) => {
         {value}
       </p>
     </>
+  );
+};
+
+const TodoChecklist = ({ text, isChecked, onChange }) => {
+  return (
+    <div className="flex items-center gap-3 p-3">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={onChange}
+        className="w-4 h-4 text-primary bg-gray-100 border-gray-300 rounded-sm outline-none cursor-pointer"
+      />
+
+      <p className="text-[13px] md:text-sm text-gray-800">{text}</p>
+    </div>
   );
 };
